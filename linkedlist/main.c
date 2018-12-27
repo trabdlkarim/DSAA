d4fc9e5d4058c17382ae1b137d14098e8cbce2f9
trabdlkarim <trabdlkarim@programmer.net>
2018-12-27 12:41:51 +0300

    update main function for more illustration

--- linkedlist/main.c
+++ linkedlist/main.c
@@ -13,28 +13,36 @@
  * 
  */
 int main(int argc, char** argv) {
-    Node *list_head=NULL;
     
+    Node* list_head = NULL;
+    
+    printf("Initial list state: ");
     add_node_tail(20,&list_head);
     add_node_tail(10,&list_head);
     add_node_tail(15,&list_head);
-    add_node_tail(50,&list_head);
-    add_node_tail(1,&list_head);
-    
     print_list(list_head);
-    /*
+    
+    printf("List state after adding elements to head: ");
+    add_node_head(50,&list_head);
+    add_node_head(1,&list_head);
+    print_list(list_head);
+    
+    printf("List state after sorting: ");
     sort_list(&list_head);
     print_list(list_head);
-     
+    
+    printf("List state after deletion of 15: ");
     delete_node(15,&list_head);
     print_list(list_head);
-     
+    
+    printf("List state after reversal:"); 
     reverse_list(&list_head);
     print_list(list_head);
     
+    printf("List state after reversal:"); 
     reverse_list(&list_head);
     print_list(list_head);
-    */
+    
     return (EXIT_SUCCESS);
 }
 
