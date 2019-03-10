6110bbfb1c9dd851248d642242383ffd01ba4f38
trabdlkarim <trabdlkarim@programmer.net>
2019-03-10 23:31:44 +0300

    first commit

---
+++ dynamicstack/include/dynamicstack.h
@@ -0,0 +1,23 @@
+
+/* 
+ * File:   dynamicstack.h
+ * Author: trabdlkarim
+ *
+ */
+
+#ifndef DYNAMICSTACK_H
+#define DYNAMICSTACK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DYNAMICSTACK_H */
+
