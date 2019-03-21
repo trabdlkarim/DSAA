8901c7da3c0711dcbb2e3cabfd267a416cad55d0
trabdlkarim <trabdlkarim@programmer.net>
2019-03-21 23:31:44 +0300

    update headers file

--- dynamicstack/include/dynamicstack.h
+++ dynamicstack/include/dynamicstack.h
@@ -12,8 +12,22 @@
 extern "C" {
 #endif
 
+#define SENTINEL -100000000
 
+typedef struct node{
+   int num;
+   struct node *link;
+}Node;
 
+Node* create_node(int value);
+
+void push(int value,Node **head);
+int pop(Node**head);
+int peek(Node* head);
+
+int is_stack_full(Node *head);
+
+void print_stack(Node* head);
 
 #ifdef __cplusplus
 }
