b7471791493d31bb18994035f9b153d0db4f527f
trabdlkarim <trabdlkarim@programmer.net>
2019-03-21 23:31:44 +0300

    update main function

--- dynamicstack/main.c
+++ dynamicstack/main.c
@@ -6,11 +6,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "include/dynamicstack.h"
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    Node* head = NULL;
+    push(100,&head);
+    push(20,&head);
+    push(60,&head);
+    push(70,&head);
+    push(80,&head);
+    push(50,&head);
+    push(10,&head);
+    push(90,&head);
+    print_stack(head);
+    
+    
+    int popped = pop(&head);
+    if(popped != SENTINEL) printf(" %4d \n",popped);
+    print_stack(head);
+    
     return (EXIT_SUCCESS);
 }
 
