8f17177f1c044762ff9c88594cbd6b6f90fba2c4
trabdlkarim <trabdlkarim@programmer.net>
2019-05-22 19:30:44 +0300

    update header

--- avl/include/avltree.h
+++ avl/include/avltree.h
@@ -20,9 +20,16 @@ extern "C" {
 
 int max(int a, int b);
 
-Node* creat_node(int key);
+Node* create_node(int key);
 
+Node* right_rotate(Node *y);
+Node* left_rotate(Node* x);
+Node *insert(Node*node, int key);
+int get_balance(Node* node);
 
+Node* get_min_value_node(Node* root);
+void preorder(Node* root);
+void preorder_helper(Node* node);
 #ifdef __cplusplus
 }
 #endif
