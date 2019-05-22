ad154a752c91306af1139c9218546165913f3776
trabdlkarim <trabdlkarim@programmer.net>
2019-05-22 19:30:44 +0300

    update lib

--- avl/lib/avltree.c
+++ avl/lib/avltree.c
@@ -11,3 +11,12 @@
 int max(int a, int b){
     return a>b ? a:b;
 }
+
+Node* creat_node(int key){
+    struct node *node=(struct node*)malloc(sizeof(struct node));
+    node->key=key;
+    node->left=node->right=NULL;
+    node->height=1;
+    return node;
+}
+
