cb1e5530147365782aa864841196cc7928259937
trabdlkarim <trabdlkarim@programmer.net>
2019-05-22 19:30:44 +0300

    add lib

---
+++ avl/lib/avltree.c
@@ -0,0 +1,13 @@
+/* 
+ * File:   avltree.c
+ * Author: trabdlkarim
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/avltree.h"
+
+int max(int a, int b){
+    return a>b ? a:b;
+}
