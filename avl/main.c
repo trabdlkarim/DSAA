87f1bd47f199d1341e8874d101700e6e02bbe3ac
trabdlkarim <trabdlkarim@programmer.net>
2019-05-22 19:30:44 +0300

    update main func

--- avl/main.c
+++ avl/main.c
@@ -6,11 +6,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include"include/avltree.h"
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    
+    Node *root=NULL;
+    
+    root = insert(root,40);
+    root = insert(root,35);
+    root = insert(root,38);
+    root = insert(root,60);
+    root = insert(root,80);
+    root = insert(root,50);
+    root = insert(root,21);
+    
+    preorder(root);
+    
     return (EXIT_SUCCESS);
 }
 
